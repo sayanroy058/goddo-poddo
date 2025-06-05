d13e22562884349d614ebfcf1d0ffd12fee93df4
Sayan Roy <55015854+sayanroy058@users.noreply.github.com>
2025-06-06 00:46:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = (
         'mysql+pymysql://avnadmin:AVNS_xp2SiYh4HLQytUi6AmO@'
-        'mysql-a2397a7-deliveryotter-3338.h.aivencloud.com:15862/goddo_poddo_db'
+        'mysql-a2397a7-deliveryotter-3338.h.aivencloud.com:15862/goddo_poddo_db2'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
