a9a1429285028a97b388508d3b8a224dc915fab5
Sayan Roy <55015854+sayanroy058@users.noreply.github.com>
2025-06-08 21:37:08 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = (
-        'mysql+pymysql://avnadmin:AVNS_xp2SiYh4HLQytUi6AmO@'
-        'mysql-a2397a7-deliveryotter-3338.h.aivencloud.com:15862/goddo_poddo_db2'
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
