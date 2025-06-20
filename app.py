6f56e18f2b6024fe769b37f558d9c99c067e60bb
Sayan Roy <55015854+sayanroy058@users.noreply.github.com>
2025-06-20 22:39:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -560,4 +560,4 @@ def get_poems_by_user(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=8000,host='0.0.0.0')
